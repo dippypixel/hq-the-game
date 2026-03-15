7bde3372946ff91e304a9368e7ce6c55b58eafd0
dippypixel <dapixel8ter@gmail.com>
2026-03-14 23:41:22 -0500

    asfsafaswafq

---
+++ chars/Guymba/Guymba.cmd
@@ -0,0 +1,502 @@
+; The CMD file.
+;
+; Two parts: 1. Command definition and  2. State entry
+; (state entry is after the commands def section)
+;
+; 1. Command definition
+; ---------------------
+; Note: The commands are CASE-SENSITIVE, and so are the command names.
+; The eight directions are:
+;   B, DB, D, DF, F, UF, U, UB     (all CAPS)
+;   corresponding to back, down-back, down, downforward, etc.
+; The six buttons are:
+;   a, b, c, x, y, z               (all lower case)
+;   In default key config, abc are are the bottom, and xyz are on the
+;   top row. For 2 button characters, we recommend you use a and b.
+;   For 6 button characters, use abc for kicks and xyz for punches.
+;
+; Each [Command] section defines a command that you can use for
+; state entry, as well as in the CNS file.
+; The command section should look like:
+;
+;   [Command]
+;   name = some_name
+;   command = the_command
+;   time = time (optional)
+;   buffer.time = time (optional)
+;
+; - some_name
+;   A name to give that command. You'll use this name to refer to
+;   that command in the state entry, as well as the CNS. It is case-
+;   sensitive (QCB_a is NOT the same as Qcb_a or QCB_A).
+;
+; - command
+;   list of buttons or directions, separated by commas. Each of these
+;   buttons or directions is referred to as a "symbol".
+;   Directions and buttons can be preceded by special characters:
+;   slash (/) - means the key must be held down
+;          egs. command = /D       ;hold the down direction
+;               command = /DB, a   ;hold down-back while you press a
+;   tilde (~) - to detect key releases
+;          egs. command = ~a       ;release the a button
+;               command = ~D, F, a ;release down, press fwd, then a
+;          If you want to detect "charge moves", you can specify
+;          the time the key must be held down for (in game-ticks)
+;          egs. command = ~30a     ;hold a for at least 30 ticks, then release
+;   dollar ($) - Direction-only: detect as 4-way
+;          egs. command = $D       ;will detect if D, DB or DF is held
+;               command = $B       ;will detect if B, DB or UB is held
+;   plus (+) - Buttons only: simultaneous press
+;          egs. command = a+b      ;press a and b at the same time
+;               command = x+y+z    ;press x, y and z at the same time
+;   greater-than (>) - means there must be no other keys pressed or released
+;                      between the previous and the current symbol.
+;          egs. command = a, >~a   ;press a and release it without having hit
+;                                  ;or released any other keys in between
+;   You can combine the symbols:
+;     eg. command = ~30$D, a+b     ;hold D, DB or DF for 30 ticks, release,
+;                                  ;then press a and b together
+;
+;   Note: Successive direction symbols are always expanded in a manner similar
+;         to this example:
+;           command = F, F
+;         is expanded when MUGEN reads it, to become equivalent to:
+;           command = F, >~F, >F
+;
+;   It is recommended that for most "motion" commads, eg. quarter-circle-fwd,
+;   you start off with a "release direction". This makes the command easier
+;   to do.
+;
+; - time (optional)
+;   Time allowed to do the command, given in game-ticks. The default
+;   value for this is set in the [Defaults] section below. A typical
+;   value is 15.
+;
+; - buffer.time (optional)
+;   Time that the command will be buffered for. If the command is done
+;   successfully, then it will be valid for this time. The simplest
+;   case is to set this to 1. That means that the command is valid
+;   only in the same tick it is performed. With a higher value, such
+;   as 3 or 4, you can get a "looser" feel to the command. The result
+;   is that combos can become easier to do because you can perform
+;   the command early. Attacks just as you regain control (eg. from
+;   getting up) also become easier to do. The side effect of this is
+;   that the command is continuously asserted, so it will seem as if
+;   you had performed the move rapidly in succession during the valid
+;   time. To understand this, try setting buffer.time to 30 and hit
+;   a fast attack, such as KFM's light punch.
+;   The default value for this is set in the [Defaults] section below.
+;   This parameter does not affect hold-only commands (eg. /F). It
+;   will be assumed to be 1 for those commands.
+;
+; If you have two or more commands with the same name, all of them will
+; work. You can use it to allow multiple motions for the same move.
+;
+; Some common commands examples are given below.
+;
+; [Command] ;Quarter circle forward + x
+; name = "QCF_x"
+; command = ~D, DF, F, x
+;
+; [Command] ;Half circle back + a
+; name = "HCB_a"
+; command = ~F, DF, D, DB, B, a
+;
+; [Command] ;Two quarter circles forward + y
+; name = "2QCF_y"
+; command = ~D, DF, F, D, DF, F, y
+;
+; [Command] ;Tap b rapidly
+; name = "5b"
+; command = b, b, b, b, b
+; time = 30
+;
+; [Command] ;Charge back, then forward + z
+; name = "charge_B_F_z"
+; command = ~60$B, F, z
+; time = 10
+;
+; [Command] ;Charge down, then up + c
+; name = "charge_D_U_c"
+; command = ~60$D, U, c
+; time = 10
+
+
+;-| Button Remapping |-----------------------------------------------------
+; This section lets you remap the player's buttons (to easily change the
+; button configuration). The format is:
+;   old_button = new_button
+; If new_button is left blank, the button cannot be pressed.
+[Remap]
+x = x
+y = y
+z = z
+a = a
+b = b
+c = c
+s = s
+
+;-| Default Values |-------------------------------------------------------
+[Defaults]
+; Default value for the "time" parameter of a Command. Minimum 1.
+command.time = 15
+
+; Default value for the "buffer.time" parameter of a Command. Minimum 1,
+; maximum 30.
+command.buffer.time = 1
+
+;-| Special Motions |------------------------------------------------------
+[Command]
+name = "WallPunch"
+command = /$D+y
+[Command]
+name = "Uppercut"
+command = /$D+y
+[Command]
+name = "Projectile"
+command = /$D+x
+[command]
+name = "SuperJump"
+command = D,U
+time = 2
+[command]
+name = "SuperJump"
+command = z+/$U
+time = 5
+
+;-| Double Tap |-----------------------------------------------------------
+[Command]
+name = "FF"     ;Required (do not remove)
+command = F, F
+time = 10
+
+[Command]
+name = "BB"     ;Required (do not remove)
+command = B, B
+time = 10
+
+[Command]
+name = "AirDashF"
+command = z + /$F
+time = 5
+[Command]
+name = "AirDashB"
+command = z + /$B
+time = 5
+
+;-| 2/3 Button Combination |-----------------------------------------------
+[Command]
+name = "recovery";Required (do not remove)
+command = x+y
+time = 1
+
+;-| Dir + Button |---------------------------------------------------------
+[Command]
+name = "down_a"
+command = /$D,a
+time = 1
+
+[Command]
+name = "down_b"
+command = /$D,b
+time = 1
+
+;-| Single Button |---------------------------------------------------------
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "start"
+command = s
+time = 1
+
+[Command]
+name = "up" ;Required (do not remove)
+command = U
+time = 1
+
+;-| Hold Dir |--------------------------------------------------------------
+[Command]
+name = "holdfwd";Required (do not remove)
+command = /$F
+time = 1
+
+[Command]
+name = "holdback";Required (do not remove)
+command = /$B
+time = 1
+
+[Command]
+name = "holdup" ;Required (do not remove)
+command = /$U
+time = 1
+
+[Command]
+name = "holddown";Required (do not remove)
+command = /$D
+time = 1
+
+;---------------------------------------------------------------------------
+; 2. State entry
+; --------------
+; This is where you define what commands bring you to what states.
+;
+; Each state entry block looks like:
+;   [State -1, Label]           ;Change Label to any name you want to use to
+;                               ;identify the state with.
+;   type = ChangeState          ;Don't change this
+;   value = new_state_number
+;   trigger1 = command = command_name
+;   . . .  (any additional triggers)
+;
+; - new_state_number is the number of the state to change to
+; - command_name is the name of the command (from the section above)
+; - Useful triggers to know:
+;   - statetype
+;       S, C or A : current state-type of player (stand, crouch, air)
+;   - ctrl
+;       0 or 1 : 1 if player has control. Unless "interrupting" another
+;                move, you'll want ctrl = 1
+;   - stateno
+;       number of state player is in - useful for "move interrupts"
+;   - movecontact
+;       0 or 1 : 1 if player's last attack touched the opponent
+;                useful for "move interrupts"
+;
+; Note: The order of state entry is important.
+;   State entry with a certain command must come before another state
+;   entry with a command that is the subset of the first.
+;   For example, command "fwd_a" must be listed before "a", and
+;   "fwd_ab" should come before both of the others.
+;
+; For reference on triggers, see CNS documentation.
+;
+; Just for your information (skip if you're not interested):
+; This part is an extension of the CNS. "State -1" is a special state
+; that is executed once every game-tick, regardless of what other state
+; you are in.
+
+
+; Don't remove the following line. It's required by the CMD standard.
+[Statedef -1]
+
+;===========================================================================
+;This is not a move, but it sets up var(1) to be 1 if conditions are right
+;for a combo into a special move (used below).
+;Since a lot of special moves rely on the same conditions, this reduces
+;redundant logic.
+[State -1, Combo condition Reset]
+type = VarSet
+trigger1 = 1
+var(1) = 0
+
+[State -1, Combo condition Check]
+type = VarSet
+trigger1 = statetype != A
+trigger1 = ctrl
+trigger2 = (stateno = [200,299]) || (stateno = [400,499])
+trigger2 = stateno != 440 ;Except for sweep kick
+trigger2 = movecontact
+trigger3 = stateno = 1310 || stateno = 1330 ;From blocking
+var(1) = 1
+
+;===========================================================================
+;---------------------------------------------------------------------------
+[State -1, superjump]
+type = ChangeState
+value = 9000
+triggerall = command = "SuperJump"
+trigger1 = ctrl
+trigger1 = statetype  != A
+trigger2 = stateno=410 && movehit
+trigger3 = stateno = 900 && AnimElemTime(4)>=1
+;Air DashFwd
+[State -1, Air Dash]
+type = ChangeState
+value = 110
+triggerall = !var(55)
+triggerall = var(18)<3
+triggerall = stateno != 110
+triggerall = command = "AirDashF" || command = "FF"
+triggerall = statetype = A
+trigger1 = Pos Y < -const(movement.airjump.height)
+trigger1 = ctrl 
+trigger2 = movehit
+;Air DashFwd
+[State -1, Air Dash]
+type = ChangeState
+value = 115
+triggerall = !var(55)
+triggerall = var(18)<3
+triggerall = stateno != 115
+triggerall = command = "AirDashB" || command = "BB"
+triggerall = statetype = A
+trigger1 = Pos Y < -const(movement.airjump.height)
+trigger1 = ctrl 
+trigger2 = movehit
+;Run Fwd
+[State -1, Run Fwd]
+type = ChangeState
+value = 100
+trigger1 = command = "AirDashF" || command = "FF"
+trigger1 = statetype != A
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Run Back
+[State -1, Run Back]
+type = ChangeState
+value = 105
+trigger1 = command = "AirDashB" || command = "BB"
+trigger1 = statetype != A
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Crouching Strong Punch
+[State -1, Projectile]
+type = ChangeState
+value = 900
+triggerall = command = "Projectile"
+triggerall = numhelper(901) <= 2
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = prevstateno != 900 && stateno = 900 && AnimElemTime(4)>=1
+
+;---------------------------------------------------------------------------
+;Crouching Strong Punch
+[State -1, Uppercut]
+type = ChangeState
+value = 410
+triggerall = command = "Uppercut"
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = stateno = [200,240]
+trigger2 = movecontact
+
+;Jump Strong Punch
+[State -1, WallPunch]
+type = ChangeState
+value = 650
+triggerall = command = "WallPunch"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = [600,620] ;jump_x or jump_a
+trigger2 = movecontact
+trigger3 = stateno = 640 ;jump_x or jump_a
+trigger3 = movecontact&& time>10
+;===========================================================================
+;---------------------------------------------------------------------------
+;Stand Light Punch
+[State -1, Stand Light Punch]
+type = ChangeState
+value = 200
+triggerall = command = "x"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = prevstateno != 200 && stateno = 200 && movecontact
+
+;---------------------------------------------------------------------------
+;Standing Strong Kick
+[State -1, Stand Medium Punch]
+type = ChangeState
+value = 210
+triggerall = command = "y"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = (stateno = 200) && movehit
+trigger3 = (stateno = 230) && movehit
+;---------------------------------------------------------------------------
+;Stand Light Punch
+[State -1, Stand Light Kick]
+type = ChangeState
+value = 230
+triggerall = command = "a"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = prevstateno != 230 && stateno = 230 && movecontact
+trigger3 = stateno = 200 && movecontact
+;---------------------------------------------------------------------------
+;Stand Strong Punch
+[State -1, Stand Strong Kick]
+type = ChangeState
+value = 240
+triggerall = command = "b"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = (stateno = 200) && movehit
+trigger3 = (stateno = 210) && movehit
+trigger4 = (stateno = 230) && movehit
+;---------------------------------------------------------------------------
+;Jump Light Punch
+[State -1, Jump Light Punch]
+type = ChangeState
+value = 600
+triggerall = command = "x"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = prevstateno != 600 && stateno = 600 && movecontact
+trigger3 = stateno = 640 ;jump_x or jump_a
+trigger3 = movecontact&& time>10
+trigger4 = enemynear,stateno=5820
+
+;---------------------------------------------------------------------------
+;Jump Strong Punch
+[State -1, Jump Medium Punch]
+type = ChangeState
+value = 610
+triggerall = command = "y"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 600 || stateno = 630 ;jump_x or jump_a
+trigger2 = movecontact
+trigger3 = stateno = 640 && movehit && time > 10 ;Air blocking
+
+;---------------------------------------------------------------------------
+
+;---------------------------------------------------------------------------
+;Jump Light Punch
+[State -1, Jump Light Punch]
+type = ChangeState
+value = 630
+triggerall = command = "a"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = prevstateno != 630 && stateno = 630 && movecontact
+trigger3 = stateno = 600 && movehit 
+;Jump Strong Kick
+[State -1, Jump Strong Punch]
+type = ChangeState
+value = 640
+triggerall = command = "b"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 600 || stateno = 630|| stateno = 610 ;jump_x or jump_a
+trigger2 = movecontact
+trigger3 = enemynear,stateno=5820
